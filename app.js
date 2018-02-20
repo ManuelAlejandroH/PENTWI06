94d6c21608a68407bd5544098c47461bee91e7cf
Manuel <manuel.herrera@softtek.com>
2018-02-20 07:52:30 -0800

    Proyecto AngularJs

---
+++ app.js
@@ -0,0 +1,8 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller('LibrosCtrl', function ($scope) {
+    $scope.Libro = {
+        title: "1908",
+        tagLine: "Todos los libros"
+    }
+});
