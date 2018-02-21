fd0359ead8fbf3b38fe0a61e628af9d074f552d4
ManuelAlejandroH <alex.herrera@outlook.com>
2018-02-20 22:09:44 -0800

    version

--- app.js
+++ app.js
@@ -3,17 +3,21 @@ var myApp = angular.module('myApp', []);
 myApp.controller('LibrosCtrl', 
 function ($scope, $http) {
     var urlLibros = 'https://www.googleapis.com/books/v1/volumes?q=' + $scope.criterio;
-    var titulo = '';
+    
     
     $scope.cargarLibros = function () { 
         $http.get(urlLibros)
         .then(function(success){
-     
+            console.log(success.data.items[1].id);
+            var test = success.data.items;
+            console.log(test[1].id);
+        for(var i =0; i < test.lenght; i++){
            
-               $scope.titulo = success;
+            console.log(test.lenght);
+        }              
          
         }, function(error){
-
+          console.log(error);
         })
     };
 });
